1fe0c4916c852d9ee9718488dbb774e1603f4d24
bgeethagayathri2-del <bgeethagayathri2@gmail.com>
2026-04-15 11:22:18 +0530

    UC3 implementation

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -1,16 +1,11 @@
-//HELLOAPP UC2
 package com.example.helloapp;
-import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        
-        // Display Hello World to console
-        System.out.println("Hello World");
-        
         if (args.length > 0) {
-            System.out.println("Helloo " + args[0]);
+            System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Helllooo User");
+            System.out.println("Hello User");
         }
     }
 }
